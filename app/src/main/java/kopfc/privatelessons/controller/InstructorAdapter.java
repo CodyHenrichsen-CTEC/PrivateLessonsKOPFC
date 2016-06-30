5beafb4e012ab991aeb04dd77e44d0ec8da0f87b
CodyHenrichsen-CTEC <cody.henrichsen@canyonsdistrict.org>
2016-06-30 15:00:23 -0600

    updated recycler

--- app/src/main/java/kopfc/privatelessons/controller/InstructorAdapter.java
+++ app/src/main/java/kopfc/privatelessons/controller/InstructorAdapter.java
@@ -1,6 +1,7 @@
 package kopfc.privatelessons.controller;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -65,71 +66,51 @@ public class InstructorAdapter extends ArrayAdapter<Instructor>
 
     public View getView(int position, View convertView, ViewGroup parent)
     {
-        try
+        InstructorHolder currentHolder=null;
+        View instructorRowItem = convertView;
+        if(instructorRowItem == null)
         {
-
-            View instructorRowItem = inflater.inflate(R.layout.instructor_row_view, null);
-            final InstructorHolder currentHolder;
-
-
-
-            Instructor current = getItem(position);
-            String name = current.getName().replaceAll("\\s","").toLowerCase();
-
-            currentHolder = new InstructorHolder();
-            currentHolder.nameView = (TextView) instructorRowItem.findViewById(R.id.instructor_name_text);
-            currentHolder.bioView = (TextView) instructorRowItem.findViewById(R.id.instructor_bio_text);
-            currentHolder.instructorImage = (ImageView) instructorRowItem.findViewById(R.id.instructor_image);
-
-            currentHolder.nameView.setText(current.getName());
-            currentHolder.bioView.setText(current.getBiography());
-            int instructorImageResource = getContext().getResources().getIdentifier(name, "drawable", getContext().getPackageName());
-
-            if(instructorImageResource != 0)
+            try
             {
-                currentHolder.instructorImage.setImageResource(instructorImageResource);
+                instructorRowItem = inflater.inflate(R.layout.instructor_row_view, null);
+
+                currentHolder = new InstructorHolder();
+
+                currentHolder.nameView = (TextView) instructorRowItem.findViewById(R.id.instructor_name_text);
+                currentHolder.bioView = (TextView) instructorRowItem.findViewById(R.id.instructor_bio_text);
+                currentHolder.instructorImage = (ImageView) instructorRowItem.findViewById(R.id.instructor_image);
+
+                instructorRowItem.setTag(currentHolder);
             }
-            else
+            catch (Exception error)
             {
-                currentHolder.instructorImage.setImageResource(R.drawable.noname);
+
             }
-
-//            if(convertView == null)
-//            {
-//                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//                instructorRowItem = inflater.inflate(R.layout.instructor_row_view, null);
-//
-//                currentHolder = new InstructorHolder();
-//                currentHolder.nameView = (TextView) instructorRowItem.findViewById(R.id.instructor_name_text);
-//                currentHolder.bioView = (TextView) instructorRowItem.findViewById(R.id.instructor_bio_text);
-//                currentHolder.instructorImage = (ImageView) instructorRowItem.findViewById(R.id.instructor_image);
-//
-//                int instructorImageResource = getContext().getResources().getIdentifier(name, "drawable", getContext().getPackageName());
-//                if(instructorImageResource != 0)
-//                {
-//                    currentHolder.instructorImage.setImageResource(instructorImageResource);
-//                }
-//                else
-//                {
-//                    currentHolder.instructorImage.setImageResource(R.drawable.noname);
-//                }
-//
-//                instructorRowItem.setTag(currentHolder);
-//            }
-//            else
-//            {
-//                currentHolder = (InstructorHolder) instructorRowItem.getTag();
-//                instructorRowItem = convertView;
-//            }
-
-            return instructorRowItem;
-
-
         }
-        catch (Exception error)
+        else
         {
-            return null;
+            currentHolder = (InstructorHolder) instructorRowItem.getTag();
         }
+
+        Instructor current = getItem(position);
+        String name = current.getName().replaceAll("\\s", "").toLowerCase();
+
+        currentHolder.nameView.setText(current.getName());
+        currentHolder.bioView.setText(current.getBiography());
+        int instructorImageResource = getContext().getResources().getIdentifier(name, "drawable", getContext().getPackageName());
+
+        if (instructorImageResource != 0)
+        {
+            currentHolder.instructorImage.setImageResource(instructorImageResource);
+        }
+        else
+        {
+            currentHolder.instructorImage.setImageResource(R.drawable.noname);
+        }
+
+
+        return instructorRowItem;
     }
 
 }
+
